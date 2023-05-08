ee4e57bef1e2edb9149c3b8cf9467f7b9564ff8d
Laurenz Rausche <laurenz@laurenz-rausche.de>
2023-05-08 08:27:21 +0200

    add api implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [apiResponse, setApiResponse] = useState("Loading");
+
+  useEffect(() => {
+    fetch("/api/v1/ping")
+      .then((res) => res.text())
+      .then((res) => setApiResponse(res));
+  }, []);
   return (
     <>
-      <p>API HERE SOON</p>
+      <p>API Response: {apiResponse}</p>
     </>
   );
 }
